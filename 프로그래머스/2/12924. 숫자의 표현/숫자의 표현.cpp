305369ccee7686140fce2ceed2fa7b661259ce1f
지훈 <93636727+nsa05605@users.noreply.github.com>
2024-05-22 15:23:33 +0900

    [level 2] Title: 숫자의 표현, Time: 0.03 ms, Memory: 3.84 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(int n){
+    int answer = 1;
+    for (int i=1; i<n; i++){
+        int sum = 0, start = i;
+        while(sum < n) {sum += start++;}
+        if (sum == n) {answer++;}
+    }
+    return answer;
+}
